f5aecb5068b224957a94a94a71292ae6c9fa8eca
VK <belsynth@gmail.com>
2021-06-08 09:21:44 +0300

    Урок Модифицирующие формы

--- public/index.php
+++ public/index.php
@@ -1,16 +1,75 @@
 <?php
 
+use Slim\Factory\AppFactory;
+use DI\Container;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-use Slim\Factory\AppFactory;
+$container = new Container();
+$container->set(
+    'renderer',
+    function () {
+        return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+    }
+);
 
-$app = AppFactory::create();
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
-});
+$users = json_decode(file_get_contents('files/users.json'), true);
+
+$app->get(
+    '/users',
+    function ($request, $response) use ($users) {
+        $term = $request->getQueryParam('term');
+
+        if (!empty($term) && !empty($users)) {
+            $names = array_filter(
+                $users,
+                fn($user) => strpos(strtolower($user['nickname']), strtolower($term)) !== false
+            );
+        }
+
+        $params = [
+            'users' => $users ?? [],
+            'names' => $names ?? [],
+            'term' => $term,
+        ];
+
+        return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+    }
+);
+
+$app->get(
+    '/users/{id:[0-9]+}',
+    function ($request, $response, $args) use ($users) {
+        $id = $args['id'];
+        $params = [
+            'id' => $id,
+            'nickname' => $users[$id]['nickname'],
+        ];
+
+        return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+    }
+);
+
+$app->get(
+    '/users/new',
+    function ($request, $response) {
+        return $this->get('renderer')->render($response, 'users/new.phtml');
+    }
+);
+
+$app->post(
+    '/users',
+    function ($request, $response) use ($users) {
+        $userData = $request->getParsedBodyParam('user');
+        $id = mt_rand(1, 100);
+        $users[$id] = $userData;
+        file_put_contents('files/users.json', json_encode($users, JSON_PRETTY_PRINT));
+
+        return $response->withRedirect('/users', 302);
+    }
+);
+
 $app->run();
