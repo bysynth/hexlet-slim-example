800a504c04564cb0843ff448a2811e3f8476327d
VK <belsynth@gmail.com>
2021-06-10 14:11:56 +0300

    Урок CRUD создание

--- public/index.php
+++ public/index.php
@@ -2,8 +2,11 @@
 
 use Slim\Factory\AppFactory;
 use DI\Container;
+use App\Validator;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+session_start();
 
 $users = json_decode(file_get_contents('files/users.json'), true);
 
@@ -14,8 +17,16 @@ $container->set(
         return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
     }
 );
+$container->set(
+    'flash',
+    function () {
+        return new \Slim\Flash\Messages();
+    }
+);
 
-$app = AppFactory::createFromContainer($container);
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
 $app->addErrorMiddleware(true, true, true);
 
 $router = $app->getRouteCollector()->getRouteParser();
@@ -30,6 +41,7 @@ $app->get(
 $app->get(
     '/users',
     function ($request, $response) use ($users) {
+        $messages = $this->get('flash')->getMessages();
         $term = $request->getQueryParam('term');
 
         if (!empty($term) && !empty($users)) {
@@ -40,6 +52,7 @@ $app->get(
         }
 
         $params = [
+            'flash' => $messages ?? [],
             'users' => $users ?? [],
             'names' => $names ?? [],
             'term' => $term,
@@ -53,18 +66,36 @@ $app->post(
     '/users',
     function ($request, $response) use ($users, $router) {
         $userData = $request->getParsedBodyParam('user');
-        $id = uniqid();
-        $users[$id] = $userData;
-        file_put_contents('files/users.json', json_encode($users, JSON_PRETTY_PRINT));
 
-        return $response->withRedirect($router->urlFor('user', ['id' => $id]), 302);
+        $validator = new Validator();
+        $errors = $validator->validate($userData);
+
+        if (count($errors) === 0) {
+            $id = uniqid();
+            $users[$id] = $userData;
+            file_put_contents('files/users.json', json_encode($users, JSON_PRETTY_PRINT));
+            $this->get('flash')->addMessage('success', "Пользователь c id $id успешно добавлен");
+
+            return $response->withRedirect($router->urlFor('users'), 302);
+        }
+
+        $params = [
+            'userData' => $userData,
+            'errors' => $errors
+        ];
+
+        return $this->get('renderer')->render($response->withStatus(422), 'users/new.phtml', $params);
     }
 );
 
 $app->get(
     '/users/new',
     function ($request, $response) {
-        return $this->get('renderer')->render($response, 'users/new.phtml');
+        $params = [
+            'userData' => [],
+            'errors' => []
+        ];
+        return $this->get('renderer')->render($response, 'users/new.phtml', $params);
     }
 )->setName('new-user');
 
@@ -72,9 +103,11 @@ $app->get(
     '/users/{id}',
     function ($request, $response, $args) use ($users) {
         $id = $args['id'];
+
         if (!array_key_exists($id, $users)) {
-            return $response->withStatus(404);
+            return $response->write('Page not found')->withStatus(404);
         }
+
         $params = [
             'id' => $id,
             'nickname' => $users[$id]['nickname'],
